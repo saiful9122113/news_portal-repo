aad1cf5f94853ddf6abc5abc97a6c05d3c3e1e1d
Md. Saiful Islam <saiful9122113@gmail.com>
2022-09-03 06:10:25 +0600

    blogs question are done

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// document.getElementById('btn-blog').addEventListener('click', function () {
+//     // const answerOfQuestion=document.getElementById('question');
+//     console.log("aaa");
+
+// })
+
+
+
+
+// const loadPhones = async () => {
+//     const url = `https://openapi.programming-hero.com/api/news/categories`
+//     // const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     displayNews(data.status);
+// }
+
+// const displayNews = phones => {
+//     console.log(data.status);
+// }
